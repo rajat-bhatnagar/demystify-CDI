b667cdbbfaa7d7fdf772aaaec0e4ddd3133f5040
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2016-12-26 01:12:23 -0500

    Added dependency and binding for slf4j and replaced SOP's with logger statements

--- src/main/java/bl/CDIApplication.java
+++ src/main/java/bl/CDIApplication.java
@@ -1,24 +1,31 @@
+package bl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import bl.service.BookService;
 import bl.service.NumberGenerator;
 import bl.service.impl.IsbnGenerator;
 import bl.service.impl.IssnGenerator;
 import data.Book;
 
-
 public class CDIApplication {
-
+	static Logger logger = LoggerFactory.getLogger(CDIApplication.class);
+	
 	public static void main(String[] args) {
 		
 		NumberGenerator isbnGenerator = new IsbnGenerator();
 		NumberGenerator issnGenerator = new IssnGenerator();
-		
+		/*
+		 * We are changing the NumberGenerator Implementation at runtime
+		 * to enable loose Coupling between BookService and NumberGenerator
+		 */
 		BookService bookServiceIsbn = new BookService(isbnGenerator);
 		Book bookWithIsbn = bookServiceIsbn.createBook("Spring in Action");
-		System.out.println("bookWithIsbn # "+bookWithIsbn);
+		logger.info("bookWithIsbn # "+bookWithIsbn);
 		
 		BookService bookServiceIssn = new BookService(issnGenerator);
 		Book bookWithIssn = bookServiceIssn.createBook("Groovy in Action");
-		System.out.println("bookWithIssn # "+bookWithIssn);
+		logger.info("bookWithIssn # "+bookWithIssn);
 	}
 
 }
