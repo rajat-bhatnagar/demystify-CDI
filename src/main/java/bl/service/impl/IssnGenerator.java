baabc4629fd8ab719bf26488af1978fd9d00f443
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2016-12-27 15:35:43 -0500

    Added @Vetoed to IssnGenerator to demonstrate how we can exclude a class from CDI

--- src/main/java/bl/service/impl/IssnGenerator.java
+++ src/main/java/bl/service/impl/IssnGenerator.java
@@ -2,9 +2,15 @@ package bl.service.impl;
 
 import java.util.Random;
 
+import javax.enterprise.inject.Vetoed;
+
 import bl.qualifiers.IssnNumber;
 import bl.service.NumberGenerator;
 
+/*
+ * @Vetoed is used to prevent the processing of a class . This class will NEVER get injected
+ */
+@Vetoed
 @IssnNumber
 public class IssnGenerator implements NumberGenerator {
 
